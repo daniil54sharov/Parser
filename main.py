864c8bdd14fed369bdfb412537522b2542a23024
daniil54sharov <daniilsharov54@gmail.com>
2025-11-01 23:56:57 +0200

    writing data to xml file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import xml.etree.ElementTree as ET
 
 url = 'https://books.toscrape.com/'
 soup = None
 response = None
+root = ET.Element('book_catalog')
+id_count = 0
 
 try:
     response = requests.get(url)
@@ -28,7 +31,22 @@ if response.ok:
         rating = rating_map.get(rating_name, 0)
 
         # достаем изображение
+        image_address = None
         image_tag = book.find('img')
-        image_src = image_tag['src']
+        image_src = image_tag.get('src')
+        if image_src: # Check if src attribute exists
+            image_address = image_src
 
-        print(f"{title} : {price} : ⭐ {rating} : {image_src}")
+        id_xml = ET.SubElement(root, 'book', id = str(id_count))
+        title_xml = ET.SubElement(id_xml, 'title', book_title = title)
+        price_xml = ET.SubElement(id_xml, 'price', book_price = price)
+        image_src_xml = ET.SubElement(id_xml, 'image', book_image_src = 'https://books.toscrape.com/' + image_address)
+        rating_xml = ET.SubElement(id_xml, 'rating', book_rating = str(rating))
+
+        id_count += 1
+else:
+    print('Failed to parse data')
+
+tree = ET.ElementTree(root)
+ET.indent(tree)
+tree.write('books.xml', encoding='utf-8', xml_declaration=True)
